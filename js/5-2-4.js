7ef5479853f0086f8f6bf8455da9133dc98553f0
Maiko333777 <90162348+Maiko333777@users.noreply.github.com>
2021-12-24 14:26:50 +0900

    Add files via upload

---
+++ js/5-2-4.js
@@ -0,0 +1,30 @@
+$(function() {
+  $('.hamburger').click(function() {
+      $(this).toggleClass('active');
+
+      if ($(this).hasClass('active')) {
+          $('.globalMenuSp').addClass('active');
+      } else {
+          $('.globalMenuSp').removeClass('active');
+      }
+  });
+});
+
+// スクロールしたら上にあがる昨日
+jQuery(function() {
+  var pagetop = $('#page_top');   
+  pagetop.hide();
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+          pagetop.fadeIn();
+      } else {
+          pagetop.fadeOut();
+      }
+  });
+  pagetop.click(function () {
+      $('body,html').animate({
+          scrollTop: 0
+      }, 500); //0.5秒かけてトップへ移動
+      return false;
+  });
+});
